0357af0a4e1b67105946278f605ffa2e4c1c8660
William Brewer <william.joseph.brewer@gmail.com>
2019-01-20 22:22:33 -0500

    emit chat message to all

--- chat-example/index.js
+++ chat-example/index.js
@@ -23,9 +23,10 @@ app.get('/', function(req, res){
 io.on('connection', function(socket){
 
 	console.log('a user connected');
-	
+
 	socket.on('chat message', function(msg){
     	console.log('message: ' + msg);
+    	io.emit('chat message', msg);
   	});
   	
   	socket.on('disconnect', function(){
